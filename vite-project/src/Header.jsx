b0994f6f384e07a9841a0ec4298a8c73e0070142
kavin-111 <kavinvishnun@gmail.com>
2025-02-04 21:38:54 +0530

    three

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -15,17 +15,6 @@ const Header = () => {
             </svg>
           </button>
         </div>
-        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <div className="text-sm lg:flex-grow">
-            <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-              Blogs
-            </a>
-           
-          </div>
-          <div>
-            <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Add Blog</a>
-          </div>
-        </div>
       </nav>
     </div>
   )
